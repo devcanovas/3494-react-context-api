a129ef3a975a8f759eb3bdc607730ef44b2a484e
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-08 21:10:55 -0300

    Refatorando a contagem de itens e valor total do carrinho

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 // Criando Context para resolver o problema de prop drilling
 export const CarrinhoContext = React.createContext();
+CarrinhoContext.displayName = "Carrinho";
 
 export const CarrinhoProvider = ({ children }) => {
   const [carrinho, setCarrinho] = useState([]);
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
 
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        setCarrinho,
+        quantidade,
+        setQuantidade,
+        valorTotal,
+        setValorTotal
+      }}
+    >
       {children}
     </CarrinhoContext.Provider>
   );
--- src/hooks/useCarrinhoContext.jsx
+++ src/hooks/useCarrinhoContext.jsx
@@ -1,8 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  const {
+    carrinho,
+    setCarrinho,
+    quantidade,
+    setQuantidade,
+    valorTotal,
+    setValorTotal,
+  } = useContext(CarrinhoContext);
 
   function mudarQuantidade(id, quantidade) {
     return carrinho.map((itemDoCarrinho) => {
@@ -41,11 +48,35 @@ export const useCarrinhoContext = () => {
     setCarrinho([...carrinhoAtualizado]);
   }
 
+  function removerProdutoCarrinho(id) {
+    const produtosRestantes = carrinho.filter(
+      (itemDoCarrinho) => itemDoCarrinho.id !== id
+    );
+    setCarrinho(produtosRestantes);
+  }
+
+  useEffect(() => {
+    const { totalTemp, quantidadeTemp } = carrinho.reduce(
+      (acumulador, produto) => {
+        return {
+          quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+          totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+        };
+      },
+      { quantidadeTemp: 0, totalTemp: 0 }
+    );
+    setQuantidade(quantidadeTemp);
+    setValorTotal(totalTemp);
+  }, [carrinho]);
+
   //   Configurando o retorno do hook
   return {
     carrinho,
     setCarrinho,
     adicionarProduto,
     removerProduto,
+    removerProdutoCarrinho,
+    valorTotal,
+    quantidade
   };
 };
